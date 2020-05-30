11089ffd15ae21fd7560ea49420ff75e686af568
panshen <415391360@qq.com>
2020-05-30 12:43:30 +0800

    first commit

---
+++ FiveTest/FTButton.swift
@@ -0,0 +1,21 @@
+//
+//  FTButton.swift
+//  FiveTest
+//
+//  Created by longrise on 2020/5/28.
+//  Copyright © 2020 longrise. All rights reserved.
+//
+
+import UIKit
+
+class FTButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FiveTest/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  FiveTest
+//
+//  Created by longrise on 2020/5/28.
+//  Copyright © 2020 longrise. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
